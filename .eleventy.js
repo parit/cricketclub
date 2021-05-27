489f107e0e34ad2e276b38574b6e351907ebf409
Parit Bansal <parit.bansal@isb-sib.ch>
2021-05-27 14:43:45 +0200

    added background and navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function(config) {
-    config.addWatchTarget('assets/')
+    config.addWatchTarget('assets/');
+    config.addPassthroughCopy({ 'assets/videos': "videos" });
+    config.addPassthroughCopy({ 'assets/images': "images" });
+    config.addPassthroughCopy({ 'assets/docs': "docs" });
 }
